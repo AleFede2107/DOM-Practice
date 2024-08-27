adffed5c1de3274382b75c102657c3a4036f29c1
Federico <alegriafederico0@gmail.com>
2024-08-27 15:45:27 -0600

    :lipstick: update the style files for hero slider

--- DOM-Practice/main.js
+++ DOM-Practice/main.js
@@ -1,21 +1,10 @@
 import './CSS/style.css'
 import {productSlider} from './components/product-slider';
+import {heroSlider,SelectedImage, SelectPrev, SelectNext} from './components/heroSlider';
 
 
-document.querySelector('#app').innerHTML = `<!-- SLIDER -->
-    <section class="slider">
-        <h1>Bienvenid@</h1>
-        <img class="slider-img" src="https://www.rover.com/blog/wp-content/uploads/2018/11/golden-retriever-2061715_1920.jpg" alt="imagen-perrito">
-        
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-            <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-            <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0m3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5z"/>
-          </svg>
-        
-    </section>
-`
+document.querySelector('#app').innerHTML = heroSlider();
+
 document.querySelector('#app').innerHTML += `<!-- NAVBAR -->
     <nav class="navbar">
 
@@ -43,3 +32,24 @@ document.querySelector('#app').innerHTML += `<!-- SUBSCRIBIRSE -->
             <input type="submit" id="submit" value="Enviar">
         </form>
     </section>`
+
+    //SLIDER 
+    const next = document.getElementById('next')
+    const prev = document.getElementById('prev')
+
+    //RIGHT ARROW
+
+    SelectNext()
+
+    SelectPrev()
+
+    SelectedImage()
+
+    setInterval(() => {
+        SelectNext();
+    },5000);
+
+    //LEFT ARROW
+    
+    next.addEventListener('click', SelectNext)
+    prev.addEventListener('click', SelectPrev)
